0412aada8e3de4a2c2b0cf9c75ea9259d7b257a2
Y. S <25735979+YS-jack@users.noreply.github.com>
2025-01-12 20:07:37 +0900

    ja: added new characters, fixed typos in transcript

---
+++ updater/char_lists/get_all_chars_from_files.py
@@ -0,0 +1,88 @@
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def read_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def read_second_column_from_tsv(filepath):
+    """
+    Reads a TSV file and returns the content of the second column.
+
+    :param filepath: The path to the TSV file.
+    :return: A list of strings containing the content of the second column.
+    """
+    second_column_content = ""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            columns = line.strip().split('\t')
+            if len(columns) > 1:
+                second_column_content += columns[1]
+    return second_column_content
+
+def get_unique_characters(text):
+    return set(text)
+
+def check_characters_in_file(files_to_check, reference_file):
+    reference_text = read_file(reference_file)
+    reference_chars = get_unique_characters(reference_text)
+
+    missing_chars = set()
+    for file in files_to_check:
+        text = read_second_column_from_tsv(file)
+        file_chars = get_unique_characters(text)
+        missing_chars.update(file_chars - reference_chars)
+
+    return missing_chars
+
+def get_files_with_extension(directory, extension):
+    """
+    Fetches all files in the specified directory that end with the specified extension.
+
+    :param directory: The directory to search in.
+    :param extension: The file extension to look for.
+    :return: A list of file paths that match the specified extension.
+    """
+    files = []
+    print(f"Searching in directory: {directory} for files with extension: {extension}")
+    for root, dirs, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith(extension):
+                file_path = os.path.join(root, filename)
+                print(f"Found file: {file_path}")
+                files.append(file_path)
+    print(f"Total files found: {len(files)}")
+    return files
+
+def main():
+    extension = ".tsv"
+    print("this script will check if all characters in the files ending with '.tsv' in the specified directory are present in the reference file.")
+
+    # choose language
+    print("enter a number;")
+    LANG = ["ja"]
+    for i, lang in enumerate(LANG):
+        print(f"{i} for {lang}")
+    language_num = int(input("for the language you wish to check if all character are present in the reference file (all_char_??.txt): "))
+    language = LANG[language_num]
+
+    current_file_path = os.path.abspath(__file__)
+    target_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(current_file_path))), "draft")
+    target_dir = os.path.join(target_dir, language)
+    files = get_files_with_extension(target_dir, extension)
+    reference_file = os.path.join(os.path.dirname(current_file_path),"all_char_" + language + ".txt")
+
+    missing_chars = check_characters_in_file(files, reference_file)
+
+    accepted_characters = set("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ０１２３４５６７８９！？、。\！”＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝　")
+    missing_chars = missing_chars - accepted_characters
+    if missing_chars:
+        print("Characters not found in the reference file:")
+        for char in missing_chars:
+            print(char)
+    else:
+        print("All characters are present in the reference file.")
+
+if __name__ == "__main__":
+
+    main()
