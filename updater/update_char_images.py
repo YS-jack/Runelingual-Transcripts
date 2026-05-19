d6f2fb0973e5b669245616eaacb5aebeedc0e9a9
lck3141592654 <30349312+lck3141592654@users.noreply.github.com>
2026-05-19 20:37:36 +0800

    Optimize width of characters for Chinese

--- updater/update_char_images.py
+++ updater/update_char_images.py
@@ -9,7 +9,7 @@ from common_func import LANG
 import update_hash
 
 font_size_lang = {'ja':12, 'ru':12, 'zh':12} # max 12
-canvas_size_lang = {'ja':(12,12), 'ru':(12,12), 'zh':(12,12)} # canvas size in order of width, height.
+canvas_size_lang = {'ja':(12,12), 'ru':(12,12), 'zh':(11,12)} # canvas size in order of width, height.
 text_pad_lang = {'ja':(0,0), 'ru':(0,-1), 'zh':(0,0)} # pixels to shift the text, in order of left padding and top padding
 bg_text_pad_lang = {'ja':(1,1), 'ru':(1,1), 'zh':(1,1)} # pixels to shift the 'shadow text', in order of left padding and top padding
 # add more languages as needed
@@ -70,22 +70,26 @@ def setGoodParam(char, lang): # setting width for a specific character. add more
         return setJaFontSize(char)
     elif lang == 'ru':
         return setRuFontSize(char)
+    elif lang == 'zh':
+        return setZhFontSize(char)
     else:
         return CANV_WIDTH, CANV_HEIGHT, TEXTPAD
 
-def setGoodCharWidth(char, lang): # setting width for a specific character. add more languages as needed
-    if lang == 'zh-CN':
-        return setZhFontSize(char)
-    else:
-        return CANV_WIDTH
-
 def setZhFontSize(char):
     width = CANV_WIDTH
-    if char in ('1', '2', '3', '4', '5', '6', '7', '8', '7', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '&', '#'): # these characters wider than normal, so set their width to be wider
-        width = math.ceil(CANV_WIDTH*0.5)
-    elif char in ('，', '。', '、', '（', '(', '）', ')', '+', '-', '.', '$', '/', '*', ':'):
-        width = math.ceil(CANV_WIDTH*0.33)
-    return width
+    height = CANV_HEIGHT
+    new_text_pad = TEXTPAD
+    if char in ('O'):
+        width = math.ceil(CANV_WIDTH*0.82)
+    elif char in ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'): # these characters wider than normal, so set their width to be wider
+        width = math.ceil(CANV_WIDTH*0.72)
+    elif char in ('I', '》', '?', '%', '&', 'p'):
+        width = math.ceil(CANV_WIDTH*0.62)
+    elif char in ('$', '/', '…', '[', ']', '{', '}', '^', '<', '>', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '%', '&'):
+        width = math.ceil(CANV_WIDTH*0.52)
+    elif char in ('，', '。', '、', '（', '(', '）', ')', '+', '-', '.', '*', ':', '!', '`', '|', ';', 'l'):
+        width = math.ceil(CANV_WIDTH*0.42)
+    return width, height, new_text_pad
 
 def setRuFontSize(char):
     width, _, _ = setJaFontSize(char)
